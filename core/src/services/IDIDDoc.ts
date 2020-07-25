d8c41ac7ff16bd47ec5cd496a78d5d9033ec3975
vishwasb <vishwas.bhushan@imaginea.com>
2020-07-25 10:41:29 +0530

    formatting

--- core/src/services/IDIDDoc.ts
+++ core/src/services/IDIDDoc.ts
@@ -1,19 +1,19 @@
-import VerificationRelationShips  from './VerificationRelationShips';
+import VerificationRelationShips from './VerificationRelationShips';
 import Service from './Service';
-import Proof  from './Proof';
+import Proof from './Proof';
 import { JwsVerificatioMethod, Ed25519VerificationMethod, Secp256k1VerificationMethod } from './VerificationMethods';
 
 // Description: A DID document contains information associated with the DID, such as ways to cryptographically authenticate the DID controller, as well as services that can be used to interact with the DID subject. 
 // Properties: https://w3c.github.io/did-spec-registries/#properties
-export default interface IDIDDoc{
+export default interface IDIDDoc {
     context: string;
     id: string;
-    controller: string; 
+    controller: string;
 
     verficationRelationShip: VerificationRelationShips;
-    verificationMethod: Array<JwsVerificatioMethod| Ed25519VerificationMethod| Secp256k1VerificationMethod>
-    
-    
+    verificationMethod: Array<JwsVerificatioMethod | Ed25519VerificationMethod | Secp256k1VerificationMethod>
+
+
     services: Array<Service>;
     proof: Proof;
 
