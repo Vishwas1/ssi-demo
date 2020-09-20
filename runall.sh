32efbacf0b7995cc25ed49e25bf04dab6efd636d
vishwas1 <vishwasanand.cse@gmail.com>
2020-09-20 23:43:59 +0530

    updated the script

--- runall.sh
+++ runall.sh
@@ -11,19 +11,25 @@ mkdir -p $LOG_DIR
 
 echo 'Restarting:=============================================='
 npm run setup
+mkdir -p public
 nohup npm run start > $LOG_DIR/core.log &
 
 cd ../explorer 
-nohup npm run serve  > $LOG_DIR/explorer.log &
+npm run build
+cp -R dist/* ../core/public
+# nohup npm run serve  > $LOG_DIR/explorer.log &
 cd -
 
 cd ../studio/server
+mkdir -p public
 npm run setup
 nohup npm run start > $LOG_DIR/studio_server.log &
 cd -
 
 cd ../studio/client
-nohup npm run serve > $LOG_DIR/studio_client.log &
+npm run build
+cp -R dist/* ../server/public
+# nohup npm run serve > $LOG_DIR/studio_client.log &
 echo 'Restarting:==============================================Done!'
 
 exit
